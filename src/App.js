a2f9abf6b1ef71c65407085ee868a6b18a57d680
mpramos <mpramos78915400@gmail.com>
2022-02-03 19:45:45 -0400

    Se implementaron Rutas en el App y tambien se añadieron un header y footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-// import GoApi from './pages/GoApi.jsx'
-import {BrowserRouter,Routes,Route} from 'react-router-dom' 
+import React from "react";
+import GoApi from "./pages/GoApi.jsx";
+import Card from "./components/Card.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
-  <BrowserRouter>
-     
-  </BrowserRouter>
-return (
-  <div>
-
+  return <div>
+    <header>header</header>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Card />} />
+        <Route path="/got" element={<GoApi />} />
+      </Routes>
+    </BrowserRouter>
+    <footer>footer</footer>
   </div>
-)
-  
-
-};
+}
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
 const Card = (props) => {
     let [cont,Setcont] = useState(0)
     let aumenta=()=>{
@@ -7,6 +8,8 @@ const Card = (props) => {
     return (
         <>
         <p> {props.title}</p>
+        <a href="/got">Game of thrones </a>
+        <Link to="/got">Game Of Thrones API</Link>
         <p> {cont}</p>
         <button onClick={aumenta}>Aumenta</button>
         </>
